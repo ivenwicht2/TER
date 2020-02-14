93e4942ba4dab88ca4be145357ec45b24255159e
theo-oriol <theo.toriol.lol@gmail.com>
2020-02-14 20:47:51 +0100

    création de la fonction pred et softmax

--- pytorch/model.py
+++ pytorch/model.py
@@ -1,10 +1,9 @@
 from torchvision import models
 from image_module import *
-from torch import nn , optim 
+from torch import nn 
 import torch
 from torch.utils.data import DataLoader
 import numpy as np
-import time
 from train import *
 
 
@@ -18,8 +17,15 @@ def Convnet(model_type,classe) :
             for param in model.parameters():
                 param.requires_grad = False
                 in_features = 25088
+
         if model_type == 'densenet121' :
             model = models.densenet121(pretrained=True)
+            for param in model.parameters():
+                param.requires_grad = False
+                in_features= 1024
+
+        if model_type == 'vgg19' :
+            model = models.vgg19(pretrained=True)
             for param in model.parameters():
                 param.requires_grad = False
                 in_features= 1024
@@ -32,12 +38,40 @@ def Convnet(model_type,classe) :
             nn.Linear(460,classe),  
         )
         model.classifier = fc
-        print('done')
         return model    
 
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+def pred(model,input) :
+    support = "cuda" if torch.cuda.is_available() else "cpu"
+    if support == "cuda":
+        device = torch.device("cuda")
+    else : 
+        device = torch.device("cpu")
+
+    print("pred on ",end='')
+    print("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    input = input.to(device)
+    output = model.forward(input)
+    print(output.cpu().detach().numpy())
+    proba = softmax(output.cpu().detach().numpy())
+    predicted = np.argmax(proba)
     
+    print(predicted,proba)
+
 if __name__ == '__main__' :
 
     test = Convnet('vgg16',3)
     trainloader,testloader = import_img("DATA")
-    test = train_model(test,trainloader,testloader,10)
+    test = train_model(test,trainloader,testloader,3)
+
+    from PIL import Image
+    import torchvision.transforms.functional as TF
+
+    image = Image.open('DATA/G43/G43_KIU862_55362 2.jpg')
+    x = TF.to_tensor(image)
+    x.unsqueeze_(0)
+    pred(test,x)
