7f1ee07941fb10eae37d9956b21747ec13dc6a96
theo-oriol <theo.toriol.lol@gmail.com>
2020-03-08 13:23:54 +0100

    upload similarité et extraction représentation

---
+++ Similarité/crop.py
@@ -0,0 +1,55 @@
+from PIL import Image
+from scipy import spatial
+import pytorch
+import numpy as np 
+import os
+import pickle
+
+# Définition de la fonction de découpage
+
+def crop(image, coord):
+    """
+    @param image_path: Chemin vers l'image à découper
+    @param coords: Tuple de coordonnées x/y (x1, y1, x2, y2)
+    @param saved_location: Chemin ou il faut enregistrer l'image
+    """
+    img_cropped = []
+    for element in coord : 
+        cropped_image = image.crop(element)
+        img_cropped.append(cropped_image)
+
+    return img_cropped
+
+
+def extraction_rep(image):
+    """ extrait l'espace de représentation """
+    model = pytorch.load("save/model")
+    model.to("cuda" if torch.cuda.is_available() else "cpu")
+    simi = None
+    def hook(module, input_, output):
+        nonlocal simi
+        simi = output
+    model.classifier[1].register_forward_hook(hook)
+    output = model.forward(input)
+    return simi.cpu().detach().numpy()
+
+def simi(image,nb=6):
+    """ renvoie le path des images similaires """
+    representation_total = pickle.load(open("save/representation.npy", 'rb'))
+    representation_image,_ = extraction_rep(image)
+    _,index = representation_total[0].query(representation_image,k=nb)
+    path = []
+    for el in index :
+        path.append(representation_total[1][el])
+    return path 
+
+def stack_rep(path):
+    """ sauvegarde l'espace de représentation """
+    representation = [[],[]]
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.jpg' in file:
+                rep = extraction_rep(os.path.join(r, file))
+                representation[0].append(rep)
+                representation[1].append(os.path.join(r, file))
+    pickle.dump(representation, open('save/representation.npy', 'wb'))
