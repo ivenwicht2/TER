371f9f998390b9e19ae3b3ebb8662e1f36f7806e
Fleur09 <brunfleur@hotmail.fr>
2020-03-28 15:51:27 +0100

    Flask to html links

---
+++ Site/index2.py
@@ -0,0 +1,25 @@
+
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/Accueil')
+def accueil():
+    return render_template('Accueil.html')
+    
+@app.route('/Bibliotheque')
+def biblio():
+    return render_template('Bibliotheque.html')
+    
+@app.route('/Historique')
+def histo():
+    return render_template('Historique.html')
+    
+@app.route('/Labellisation')
+def label():
+    return render_template('Labellisation.html')
+
+if __name__ == "__main__":
+    app.run()
+    
